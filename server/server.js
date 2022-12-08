366521926632ae2fa0db291ef18dfed5bd0c292f
bdilollo <113383715+bdilollo@users.noreply.github.com>
2022-12-07 19:03:22 -0600

    refactored server.js file + created typeDefs and resolvers files in schema dir

--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -14,8 +17,20 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+})
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+};
+
+startApolloServer(typeDefs, resolvers);
