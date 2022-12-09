279e698c569d8c19b40e36691adb7b2ddb8512b7
bdilollo <113383715+bdilollo@users.noreply.github.com>
2022-12-08 20:35:08 -0600

    final bug fixes

---
+++ client/build/precache-manifest.ff6e31c0e36ee6ff62e5449fb5f62bbe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf935a538cfb2f2e8d26fc1e91ce230d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97ddda4ba3b140718abe",
+    "url": "/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "125a23282d0b64f65dbd",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "97ddda4ba3b140718abe",
+    "url": "/static/js/2.4579eee5.chunk.js"
+  },
+  {
+    "revision": "35f1a3ca561e93b64bcba705f76bd30f",
+    "url": "/static/js/2.4579eee5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "125a23282d0b64f65dbd",
+    "url": "/static/js/main.9916b270.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.9916b270.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
