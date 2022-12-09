a5b06f93ff195634c99bf1992806e3d631e23f71
bdilollo <113383715+bdilollo@users.noreply.github.com>
2022-12-08 19:53:55 -0600

    modified login and signup form components

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,8 +1,7 @@
 import { useMutation } from '@apollo/client';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
-// import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
